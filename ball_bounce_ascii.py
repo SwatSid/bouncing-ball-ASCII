c5112851f63fc5c6379f3041a852845e24b0a0ef
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-07-16 07:41:41 -0700

    Added functions

--- ball_bounce_ascii.py
+++ ball_bounce_ascii.py
@@ -3,24 +3,35 @@ import time
 g = -9.8
 u = 0
 dt = 0.06
-# dt = 0.001
 
 s_o = 1.5
-while True:
-    
+
+def simulate_timestep(s_o, u, g, dt):
     v = u + g*dt
     s = s_o + u*dt + (g*dt**2)/2
     
-    # print(s," = ",s_o," + ",u,"*",dt," + (",g,"*",dt,"**2)/2)")
-    pos = int(s * 100)
+    return s, v
     
-    # print(s, pos)
+def display_bounce(pos):
     for i in range(pos):
         print(" ", end='')
     print("o")
     
+while True:
+    
+    s, v = simulate_timestep(s_o, u, g, dt)
+    
+    # variable to facilitate printing ascii ball
+    pos = int(s * 100)
+    
+    if pos < 0:
+        pos = 0
+        
+    display_bounce(pos)
+    
     u = v
     
+    # Bounce condition
     if (s <= 0 and s_o >= 0):
         u = -u*0.95
         
