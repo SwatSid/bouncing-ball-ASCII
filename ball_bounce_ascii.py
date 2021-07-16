fef31e6fc3a89662e92b9bc77f457e26ed5539dd
MSI\adsid <a.d.siddhaarthan@gmail.com>
2021-07-16 08:54:57 -0700

    Added coeff of restitution e

--- ball_bounce_ascii.py
+++ ball_bounce_ascii.py
@@ -1,12 +1,12 @@
 import time
 
-def simulate_timestep(s_o, u, g, dt):
+def simulate_timestep(s_o, u, g, dt, e):
     v = u + g*dt
     s = s_o + u*dt + (g*dt**2)/2
     
     # Bounce condition
     if (s <= 0 and s_o >= 0):
-        v = -v*0.95
+        v = -v*e
         
     return s, v
     
@@ -24,7 +24,7 @@ def main():
     # Simulation conditions
     g = -9.8
     dt = 0.06
-    
+    e = 0.7
     pos_count = 0
     
     while True:
@@ -39,7 +39,7 @@ def main():
         
         display_bounce(pos)
         
-        s, v = simulate_timestep(s, v, g, dt)
+        s, v = simulate_timestep(s, v, g, dt, e)
         
         # Time delay to keep simulation slow enuf for human eye to follow
         time.sleep(0.01)
